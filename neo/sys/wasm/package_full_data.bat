7141e61fe8e6673d01dcaea9b6f0ff7431ac1f6d
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2026-06-14 10:42:27 +0200

    added package_full_data script and fixed package* scripts to use python3

---
+++ neo/sys/wasm/package_full_data.bat
@@ -0,0 +1,10 @@
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak000.data --preload %1/pak000.pk4@/usr/local/share/d3wasm/base/pak000.pk4 --js-output=pak000.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak001.data --preload %1/pak001.pk4@/usr/local/share/d3wasm/base/pak001.pk4 --js-output=pak001.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak002.data --preload %1/pak002.pk4@/usr/local/share/d3wasm/base/pak002.pk4 --js-output=pak002.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak003.data --preload %1/pak003.pk4@/usr/local/share/d3wasm/base/pak003.pk4 --js-output=pak003.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak004.data --preload %1/pak004.pk4@/usr/local/share/d3wasm/base/pak004.pk4 --js-output=pak004.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak005.data --preload %1/pak005.pk4@/usr/local/share/d3wasm/base/pak005.pk4 --js-output=pak005.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak006.data --preload %1/pak006.pk4@/usr/local/share/d3wasm/base/pak006.pk4 --js-output=pak006.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak007.data --preload %1/pak007.pk4@/usr/local/share/d3wasm/base/pak007.pk4 --js-output=pak007.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py pak008.data --preload %1/pak008.pk4@/usr/local/share/d3wasm/base/pak008.pk4 --js-output=pak008.js --use-preload-cache --no-heap-copy
+
