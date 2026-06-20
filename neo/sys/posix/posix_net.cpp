f92d16c489729c65dd0e5f0c16a83882ea540b92
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2026-06-20 18:49:40 +0200

    cleanup in posix_net

--- neo/sys/posix/posix_net.cpp
+++ neo/sys/posix/posix_net.cpp
@@ -691,7 +691,7 @@ int idTCP::Read(void *data, int size) {
 		return -1;
 	}
 
-#if defined(_GNU_SOURCE)&&!defined(__EMSCRIPTEN__)
+#if defined(_GNU_SOURCE)
 	// handle EINTR interrupted system call with TEMP_FAILURE_RETRY -  this is probably GNU libc specific
 	if ( ( nbytes = TEMP_FAILURE_RETRY( read( fd, data, size ) ) ) == -1 ) {
 #else
@@ -752,7 +752,7 @@ int	idTCP::Write(void *data, int size) {
 		return -1;
 	}
 
-#if defined(_GNU_SOURCE)&&!defined(__EMSCRIPTEN__)
+#if defined(_GNU_SOURCE)
 	// handle EINTR interrupted system call with TEMP_FAILURE_RETRY -  this is probably GNU libc specific
 	if ( ( nbytes = TEMP_FAILURE_RETRY ( write( fd, data, size ) ) ) == -1 ) {
 #else
