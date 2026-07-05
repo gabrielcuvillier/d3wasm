b4785cd9cdf6156980df15c5e493f60219028b6c
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2026-07-05 15:54:15 +0200

    Disabled some snd Cvars

--- neo/sound/snd_system.cpp
+++ neo/sound/snd_system.cpp
@@ -39,6 +39,8 @@ idCVar idSoundSystemLocal::s_useOcclusion( "s_useOcclusion", "1", CVAR_SOUND | C
 idCVar idSoundSystemLocal::s_maxSoundsPerShader( "s_maxSoundsPerShader", "0", CVAR_SOUND | CVAR_ARCHIVE, "", 0, 10, idCmdSystem::ArgCompletion_Integer<0,10> );
 #ifndef __EMSCRIPTEN__
 idCVar idSoundSystemLocal::s_showLevelMeter( "s_showLevelMeter", "0", CVAR_SOUND | CVAR_BOOL, "" );
+#else
+idCVar idSoundSystemLocal::s_showLevelMeter( "s_showLevelMeter", "0", CVAR_SOUND | CVAR_BOOL | CVAR_ROM, "" );
 #endif
 idCVar idSoundSystemLocal::s_constantAmplitude( "s_constantAmplitude", "-1", CVAR_SOUND | CVAR_FLOAT, "" );
 idCVar idSoundSystemLocal::s_minVolume6( "s_minVolume6", "0", CVAR_SOUND | CVAR_FLOAT, "" );
