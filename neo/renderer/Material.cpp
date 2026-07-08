3e59790562aedab6616008ad2291ab62189d361d
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2026-07-08 20:44:48 +0200

    reintegrated some lastest changes from dhewm3: Renderer (very minor changes)

--- neo/renderer/Material.cpp
+++ neo/renderer/Material.cpp
@@ -2080,10 +2080,9 @@ void idMaterial::ParseMaterial( idLexer &src ) {
 	if ( cullType == CT_TWO_SIDED ) {
 		for ( i = 0 ; i < numStages ; i++ ) {
 			if ( pd->parseStages[i].lighting != SL_AMBIENT || pd->parseStages[i].texture.texgen != TG_EXPLICIT ) {
-				if ( cullType == CT_TWO_SIDED ) {
 					cullType = CT_FRONT_SIDED;
 					shouldCreateBackSides = true;
-				}
+
 				break;
 			}
 		}
