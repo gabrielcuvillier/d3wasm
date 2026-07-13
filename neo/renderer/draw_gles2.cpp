2b19f484fbfe57a821a1555ff1364c65abe6f87c
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2026-07-13 18:59:59 +0200

    For now, disable again postprocessing capture

--- neo/renderer/draw_gles2.cpp
+++ neo/renderer/draw_gles2.cpp
@@ -2083,6 +2083,10 @@ int RB_GLSL_DrawShaderPasses(drawSurf_t** drawSurfs, int numDrawSurfs) {
     return numDrawSurfs;
   }
 
+#ifndef WEBGL
+  // For now, the only postprocess materials are HeatHaze effects, which are not supported
+  // Let's disable this for now
+
   // if we are about to draw the first surface that needs
   // the rendering in a texture, copy it over
   if ( drawSurfs[0]->material->GetSort() >= SS_POST_PROCESS ) {
@@ -2101,6 +2105,7 @@ int RB_GLSL_DrawShaderPasses(drawSurf_t** drawSurfs, int numDrawSurfs) {
 
     backEnd.currentRenderCopied = true;
   }
+#endif
 
   ////////////////////////////////////////
   // GL shader setup for the current pass
